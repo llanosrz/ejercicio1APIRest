1b2db2f49626acd5a2ff3240e2e64c20c77e0673
llruiz <llanosrzm@gmail.com>
2023-03-30 14:51:29 +0200

    Estado inicial

---
+++ src/main/java/com/example/entities/Hotel.java
@@ -0,0 +1,11 @@
+package com.example.entities;
+
+import java.io.Serializable;
+
+public class Hotel implements Serializable{
+    
+    public static final long serialVersionUID = 1L;
+
+    private int id;
+    private String nombre;
+}
